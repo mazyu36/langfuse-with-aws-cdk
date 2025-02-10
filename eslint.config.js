5f16a5acdc4c2c060d38e6e4b2f4e6a0062a9c19
Matsuda <yiyth.fcb6@gmail.com>
2025-02-10 11:23:53 +0900

    chore: update to eslint v9 (#24)

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const pluginJs = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
+
+module.exports = [
+  { files: ["**/*.ts"] },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      ...prettier.rules,
+    },
+  },
+  {
+    ignores: ["**/*.d.ts"],
+  },
+];
